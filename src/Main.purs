b31fbd480460e1a7e123ab7e1fffebc7881a92c8
Marc Cordova <mcordova47@gmail.com>
2025-09-21 14:59:37 -0400

    add withTimeTravel back in

--- src/Main.purs
+++ src/Main.purs
@@ -12,7 +12,7 @@ import TimeTravel (withTimeTravel)
 
 main :: DebugWarning => Effect Unit
 main = defaultMain
-  { def: { init, update, view }
+  { def: withTimeTravel { init, update, view }
   , elementId: "app"
   }
 
